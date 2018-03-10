42fcbe57e173dc99f164b6aa95a1a27828ffe9ec
no <no@no.com>
2018-03-10 12:02:18 +0000

    print mean & std dev, before and after normalization

--- poc.py
+++ poc.py
@@ -6,6 +6,7 @@ from pyspark.sql.types import FloatType
 from pyspark.mllib.feature import StandardScaler, StandardScalerModel
 from pyspark.mllib.linalg import Vector, Vectors, DenseVector
 from pyspark.mllib.regression import LabeledPoint
+import pandas as pd
 
 
 def get_feature_json():
@@ -19,15 +20,37 @@ def get_df(data):
     return SparkSession.builder.getOrCreate().createDataFrame(data)
 
 
+def get_mean_std(pdf, start_idx):
+    agg = pd.concat([pdf[x] for x in pdf.columns[start_idx:]])
+    return agg.mean(), agg.std(), agg.size
+
+
+def fmt_mean_std(pdf, start_idx):
+    stats = get_mean_std(pdf, start_idx)
+    return 'mean: {}, std dev: {}, size: {}'.format(stats[0], stats[1], stats[2])
+
+
+def plot_distribution(size, points):
+    import matplotlib.pyplot as plt
+    plt.plot([1]*size, points)
+    plt.show()
+
 if __name__ == '__main__':
     from_file = get_feature_json()
 
     feature_sets_data = from_file['feature_sets']
     feature_list = [[k] + v['feature_data'] for k, v in feature_sets_data.iteritems()]
     df = get_df(feature_list)
-    df.show()
+    df.show(100, False)
+
+    print fmt_mean_std(df.toPandas(), 1)
 
     rdd = df.rdd.map(lambda row: DenseVector([float(c) for c in row[1:]]))
     scaler = StandardScaler().fit(rdd)
-    for i in zip([f[0] for f in feature_list], scaler.transform(rdd).collect()):
-        print '%s: %s' % (i[0], i[1])
+    transformed = scaler.transform(rdd).collect()
+
+    for name_to_features in zip([f[0] for f in feature_list], transformed):
+        print '{}: {}'.format(name_to_features[0], name_to_features[1])
+
+    after_df = pd.DataFrame.from_records(transformed)
+    print fmt_mean_std(after_df, 0)
